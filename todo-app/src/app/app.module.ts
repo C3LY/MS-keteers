e34d91bef856e813e7ef57c9aeb494fb70b6b1ad
Chloe <cy214@sussex.ac.uk>
2020-11-27 12:40:13 +0000

    added card to mat timer

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainTimerPageComponent } from './main-timer-page/main-timer-page.component';
-import { ImportCalendarComponent } from './import-calendar/import-calendar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainTimerPageComponent} from './main-timer-page/main-timer-page.component';
+import {ImportCalendarComponent} from './import-calendar/import-calendar.component';
+import {Shared3rdPartyModule} from './shared3rd-party.module';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ImportCalendarComponent } from './import-calendar/import-calendar.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Shared3rdPartyModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
