aa5d32e4f007992225b4ab1d49592505f42b6600
Chloe <cy214@sussex.ac.uk>
2020-11-27 10:42:20 +0000

    Added simple routing to main time page

--- todo-app/src/app/app-routing.module.ts
+++ todo-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainTimerPageComponent} from "./main-timer-page/main-timer-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'timer',
+    component: MainTimerPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
