2d3302cb891fab9017d42bf0064a849ff5ec8261
Blake <jordan00789@gmail.com>
2020-11-27 22:09:43 +0000

    added calendar upload to task selecter

--- todo-app/src/app/import-calendar/import-calendar.component.ts
+++ todo-app/src/app/import-calendar/import-calendar.component.ts
@@ -1,6 +1,8 @@
 import { NullTemplateVisitor } from '@angular/compiler';
 declare var require: any
 import { Component, OnInit } from '@angular/core';
+import { ITask } from '../shared/task.model';
+import { CreatedTaskService } from '../select-tasks/select-tasks-service/created-task-service.service';
 const ICAL = require('ical.js');
 
 @Component({
@@ -13,7 +15,9 @@ export class ImportCalendarComponent implements OnInit {
   calendar: File = null;
   ICALevents = []
 
-  constructor() { }
+  tasks: ITask[];
+
+  constructor(private taskService: CreatedTaskService) {}
   
   ngOnInit() {
   }
@@ -35,7 +39,6 @@ export class ImportCalendarComponent implements OnInit {
       if (event[0] = "vevent") {
         let parsedEvent = []
         var dtstart, dtend, summary;
-        let wantedParams = ["dtstart", "dtend", "summary"]
         event[1].forEach(property => {
           if (dtstart == null && property[0] == "dtstart") {
             dtstart = [property[0], property[3]];
@@ -59,6 +62,30 @@ export class ImportCalendarComponent implements OnInit {
     })
     this.ICALevents = events;
     console.log(this.ICALevents);
+
+    events.forEach(event => {
+      console.log(event[2][1]);
+      console.log(Math.floor(event[3][1]/(1000*60*60)));
+      console.log(event[3][1]/(1000*60) % 60);
+
+      var name: string = event[2][1];
+      var hou: number = Math.floor(event[3][1]/(1000*60*60));
+      var min: number = event[3][1]/(1000*60) % 60;
+
+      let task: ITask = {
+        id: undefined,
+        name: name,
+        duration: {
+          hour: hou,
+          minutes: min
+        }
+      }
+
+      console.log(task);
+      this.taskService.saveTask(task);
+
+
+    });
   }
 }
 
