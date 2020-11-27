22f4ed37d74374ec60a157d9dfc04abf397b3875
Blake <jordan00789@gmail.com>
2020-11-27 11:25:45 +0000

    calendar component

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImportCalendarComponent } from './import-calendar/import-calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImportCalendarComponent
   ],
   imports: [
     BrowserModule,
