b441b873b86b974639dc092ef6734f43f18a61bc
Blake <jordan00789@gmail.com>
2020-11-27 18:15:39 +0000

    calendar parses .ics file

--- todo-app/src/app/import-calendar/import-calendar.component.ts
+++ todo-app/src/app/import-calendar/import-calendar.component.ts
@@ -1,4 +1,7 @@
+import { NullTemplateVisitor } from '@angular/compiler';
+declare var require: any
 import { Component, OnInit } from '@angular/core';
+const ICAL = require('ical.js');
 
 @Component({
   selector: 'app-import-calendar',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportCalendarComponent implements OnInit {
 
-  constructor() { }
+  calendar: File = null;
+  ICALevents = []
 
+  constructor() { }
+  
   ngOnInit() {
   }
 
+  handleCalendar(e) {
+    this.calendar = e.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      let parsed = ICAL.parse(fileReader.result);
+      this.setEvents(parsed);
+
+    }
+    fileReader.readAsText(this.calendar);
+  }
+
+  setEvents(calendar) {
+    let events = [];
+    calendar[2].forEach(event => {
+      if (event[0] = "vevent") {
+        let parsedEvent = []
+        var dtstart, dtend, summary;
+        let wantedParams = ["dtstart", "dtend", "summary"]
+        event[1].forEach(property => {
+          if (dtstart == null && property[0] == "dtstart") {
+            dtstart = [property[0], property[3]];
+          }
+
+          else if (dtend == null && property[0] == "dtend") {
+            dtend = [property[0], property[3]];
+          }
+          
+          else if (summary == null && property[0] == "summary") {
+            summary = [property[0], property[3]];
+          }
+        });
+
+        if (dtstart && dtend && summary) {
+          let dtduration = ["dtduration", new Date(dtend[1]).getTime() - new Date(dtstart[1]).getTime()];
+          parsedEvent = [dtstart, dtend, summary, dtduration];
+          events.push(parsedEvent);
+        }
+      }
+    })
+    this.ICALevents = events;
+    console.log(this.ICALevents);
+  }
 }
+
