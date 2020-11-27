80569c427f3c2c3188f16e5a2ebd13c70db7580a
Chloe <cy214@sussex.ac.uk>
2020-11-27 09:49:13 +0000

    Added new component - main timer page

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainTimerPageComponent } from './main-timer-page/main-timer-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainTimerPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ todo-app/src/app/main-timer-page/main-timer-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-timer-page',
+  templateUrl: './main-timer-page.component.html',
+  styleUrls: ['./main-timer-page.component.scss']
+})
+export class MainTimerPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
